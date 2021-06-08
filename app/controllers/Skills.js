f266ef2b78ce3dd1d39521879d8776445e6e00de
Thomas McLaughlin <t.mclaughlin1337@gmail.com>
2021-06-08 14:24:39 +0100

    updated skills

--- app/controllers/Skills.js
+++ app/controllers/Skills.js
@@ -7,11 +7,21 @@ var paths = {
     GetSkillsLockState: function (req, res) {
         try {
             let username = req.body.username; 
+            let skillKey = req.body.skillKey; 
+
             Auth.findUserByUsername(username).then((user) => {    	
 			if(!user) {
 				return res.status(500).send('User not found')
 			}
-            
+            let skills = user.skillsStates				
+            let isUnlocked = false;
+            skills.forEach(skill => {
+                if(skill.skillKey == skillKey) 
+                {
+                    isUnlocked = skill.unlocked
+                    console.log("Skill Found  " + skillKey + isUnlocked)
+                }
+            });
 
 
                 return res.status(200).send({})
---
+++ app/controllers/methods/skillMethods.js
@@ -0,0 +1,10 @@
+var Promise 	= require('bluebird');
+var errors		= require('./../messages/errors');
+var User 		= require('./../../models/user');
+
+
+var methods = {
+    
+};
+
+module.exports = methods 
