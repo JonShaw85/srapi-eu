f119b532a8b23f33d4ce87338f692f6bbf18b438
Thomas McLaughlin <t.mclaughlin1337@gmail.com>
2021-06-08 13:16:53 +0100

    Create Skills.js

---
+++ app/controllers/Skills.js
@@ -0,0 +1,39 @@
+
+const User = require('../models/user');
+const methods = require('./methods/profile');
+const errors = require('./messages/errors');
+const notifications = require('./messages/notifications');
+const leaderboard = require('./leaderboard')
+const Auth = require('./methods/auth')
+
+
+var paths = {
+    getSkillsLockState: function (req, res) {
+        try {
+            let username = req.body.username; 
+            Auth.findUserByUsername(username).then((user) => {
+            
+
+            
+            })
+        }
+        catch(e) {
+            console.log("Caught error ---" + e)
+			return res.status(500).send()
+        }
+    },
+    setSkillsLockState: function (req, res) {
+        try {
+            let username = req.body.username; 
+            Auth.findUserByUsername(username).then((user) => {
+            
+                
+            
+            })
+        }
+        catch(e) {
+            console.log("Caught error ---" + e)
+			return res.status(500).send()
+        }
+    }
+}
